933a7e961e1b08f9557ae62d1e9fd1939af50c54
Hermann Manuel <hermann.manuel@hotmail.com.com>
2023-03-24 16:34:53 +0100

    Strong typing and transformations

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ApiPopularResponse, Movie, Movies } from './types';
 
 @Component({
   selector: 'app-movies',
@@ -7,14 +8,10 @@ import { Component, OnInit } from '@angular/core';
     <div class="row">
       <div class="col-4 mb-2" *ngFor="let movie of movies">
         <div class="card">
-          <img
-            src="https://image.tmdb.org/t/p/w500/{{ movie.poster_path }}"
-            alt=""
-            class="card-img-top"
-          />
+          <img src="{{ movie.image }}" alt="" class="card-img-top" />
           <div class="card-body">
             <h5 class="card-title">{{ movie.title }}</h5>
-            <p>{{ movie.overview }}</p>
+            <p>{{ movie.description }}</p>
           </div>
         </div>
         <strong></strong>
@@ -24,17 +21,25 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class MoviesComponent implements OnInit {
-  movies: any[] = [];
+  movies: Movies = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    const request = this.http.get(
+    const request = this.http.get<ApiPopularResponse>(
       'https://api.themoviedb.org/3/movie/popular?api_key=798865d2515c75e1f2966a2ca026c110&language=fr-FR&page=2'
     );
 
-    request.subscribe((response: any) => {
-      this.movies = response.results;
+    request.subscribe((response) => {
+      this.movies = response.results.map((item) => {
+        return {
+          id: item.id,
+          title: item.title,
+          description: item.overview,
+          rating: item.vote_average,
+          image: 'https://image.tmdb.org/t/p/w500/' + item.poster_path,
+        };
+      });
     });
   }
 }
---
+++ src/app/movies/types.ts
@@ -0,0 +1,45 @@
+export type Movie = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  rating: number;
+};
+
+export type Movies = Movie[];
+
+export type ApiPopularResponse = {
+  /**
+   * La page actuelle
+   */
+  page: number;
+  /**
+   * Les films concernés par cette page
+   */
+  results: ApiMovie[];
+  /**
+   * Le nombre de pages totales
+   */
+  total_pages: number;
+  /**
+   * Le nombre total de films
+   */
+  total_results: number;
+};
+
+export type ApiMovie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
